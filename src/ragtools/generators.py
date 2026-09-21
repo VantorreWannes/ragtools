e035a8e89434cc352e6a046e25770e891f333cfe
Wannes Vantorre <vantorrewannes@gmail.com>
2026-09-21 15:18:02 +0200

    Implement Generators.

---
+++ src/ragtools/generators.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from functools import cached_property
+from typing import Protocol
+
+from transformers import TextGenerationPipeline, pipeline
+
+
+class Generator(Protocol):
+    def prompt(self, query: str) -> str: ...
+
+
+@dataclass(slots=True)
+class TransformersGenerator:
+    model_name: str
+    max_new_tokens: int
+
+    @cached_property
+    def model(self) -> TextGenerationPipeline:
+        return pipeline("text-generation", model=self.model_name)
+
+    def prompt(self, query: str) -> str:
+        chat = [{"role": "user", "content": query}]
+        outputs = self.model(
+            chat,
+            max_new_tokens=self.max_new_tokens,
+            do_sample=True,
+        )
+        return outputs[0]["generated_text"][-1]["content"]
